57f5f9a6729b6aceb4da08ffd45dfbe693f0af30
Nifemi01 <raphaeledafesnr@gmail.com>
2026-04-28 09:04:25 +0100

    add code to switch between dark or light mode

---
+++ src/components/themeToggle.jsx
@@ -0,0 +1,11 @@
+import { useTheme } from "../context/ThemeContext.jsx";
+import styles from "../styles/";
+export default function ThemeToggle() {
+    const { theme, toggleTheme } = useTheme();
+    const isDark = theme === 'dark';
+    return (
+        <button onClick={toggleTheme} aria-label={isDark ? 'Switch to light mode' : 'Switch to dark mode'} title={isDark ? 'Lightmode' : 'Dark mode'} className={styles.button}>
+            {isDark ? '☀️' : '🌙'}
+        </button>
+    );
+}
